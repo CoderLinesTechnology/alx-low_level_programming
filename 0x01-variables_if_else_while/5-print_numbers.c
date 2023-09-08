e910c7be705503fbd273ba8df6e2ebf82301a058
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-08 17:48:03 +0000

    Print all numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+*main - Print out all single digit numbers
+*
+*Return:0 (Success)
+*/
+int main (void)
+{
+	int i;
+
+	i = 0;
+	while (i >= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
