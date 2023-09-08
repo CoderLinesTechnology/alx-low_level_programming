737e537ea5383cd48e6ddc1a073ac79b99920999
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-08 19:47:41 +0000

    Combo 3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,10 +11,10 @@ int main(void)
 	int y;
 	int z;
 
-	for (x = 48; x <= 57; x++)
-		for (y = 0; y <= 57; y++)
+	for (x = 48; x <= 55; x++)
+		for (y = 0; y <= 56; y++)
 		{
-			for(z = 48; z <= 57; z++)
+			for (z = 48; z <= 57; z++)
 			{
 				if (y <= x)
 					continue;
