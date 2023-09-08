cf47f79806325e6305049799b3ada02d6d468b45
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-08 13:09:05 +0000

    Task to find negative and positive numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes goes there */
 
 /* betty style doc for function main goes there */
@@ -12,15 +13,15 @@ int main(void)
 	/* your code goes there */
 	if (n < 0)
 	{
-		puts("is positive");
+		printf("is positive\n");
 	}
 	else if (n == 0)
 	{
-		puts("is zero");
+		printf("is zero\n");
 	}
 	else
 	{
-		puts("is negative");
+		printf("is negative\n");
 	}
 	return (0);
 }
