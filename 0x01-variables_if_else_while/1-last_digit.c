a2ef8a587f27ac2035a65602f37d1b06fdb48383
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-08 15:02:45 +0000

    Get last digit task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+*main - Print the last digit in a number
+*and check whether it is negative or positive
+*
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	int n;
+	int ldig;
+	int neg;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	neg = n;
+	if (n < 0)
+	{
+		neg = -n;
+		ldig = neg % 10;
+		ldig = -ldig;
+	}
+	else
+		ldig = n % 10;
+	printf("Last digit of %d is %d and is ", n, ldig);
+	if (n < 0)
+	{
+		printf("less than 5");
+	}
+	else if (n == 0)
+	{
+		printf("0");
+	}
+	else
+	{
+		if (ldig > 5)
+			printf("greater than 5");
+		else
+			printf("less than 6 and not 0");
+	}
+	return (0);
+}
