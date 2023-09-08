cfa0be37cc424c5e05ffaff250dd8a702d918a8c
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-08 16:49:12 +0000

    Print lower case letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*
+*main - Print lower case letters
+*
+*Return: 0 (Success)
+*
+*/
+int main(void)
+{
+	int letter;
+	
+	letter = 97;
+	while (letter <= 122)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
