8b0bb76b1e6dccc3a8bf251d34649ef84e8b3644
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-08 20:38:13 +0000

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,21 +7,21 @@
 */
 int main(void)
 {
-	int i;
+	int x;
 	int j;
 
-	for (i = 0; i < 100; i++)
+	for (x = 0; x < 100; x++)
 	{
 		for (j = 0; j < 100; j++)
 		{
-			if (i < j)
+			if (x < j)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((x / 10) + 48);
+				putchar((x % 10) + 48);
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				if (x != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
