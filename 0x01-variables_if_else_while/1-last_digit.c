f511f5f0ea51647ec529a0e62fc7d4b642859b31
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-08 15:04:19 +0000

    Get last digit task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,18 +28,18 @@ int main(void)
 	printf("Last digit of %d is %d and is ", n, ldig);
 	if (n < 0)
 	{
-		printf("less than 5");
+		printf("less than 5\n");
 	}
 	else if (n == 0)
 	{
-		printf("0");
+		printf("0\n");
 	}
 	else
 	{
 		if (ldig > 5)
-			printf("greater than 5");
+			printf("greater than 5\n");
 		else
-			printf("less than 6 and not 0");
+			printf("less than 6 and not 0\n");
 	}
 	return (0);
 }
