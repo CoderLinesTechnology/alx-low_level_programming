35bbb9713dbbd8adc1883b2a8b9191a1a30c7ddc
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-08 13:20:27 +0000

    Task to find negative and positive numbers

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,16 +1,18 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes goes there */
 
-/* betty style doc for function main goes there */
+/**
+*main - Test for Negative and Postive Integers and also zero
+*
+*Return: 0 (Success)
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
