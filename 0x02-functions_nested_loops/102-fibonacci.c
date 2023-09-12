e05bed7e8da085e7a8e765f5481476d2a62602ca
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-12 20:48:29 +0000

    Fab

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 
-void print_fibonacci(size_t n);
+void print_fibonacci(size_t);
 
 /**
  * main - driver code
  * Return: 0
  */
-int main(void)
-{
-	print_fib(50);
-	return (0);
+int main(void) {
+    printFibonacci(50);
+    return 0;
 }
 
 /**
  * print_fibonacci - print the n fibonacci number
  * @n: range
  */
-void print_fib(size_t n)
+void printFibonacci(int n)
 {
-	size_t fib_x, fib_y, fib_i, i;
-	/* initial value */
-	fib_x = 1;
-	fib_y = 2;
+	if (n <= 0)
+ 		return;
 
-	printf("%ld, %ld, ", fib_x, fib_y);
+	unsigned long long fib_x = 1, fib_y = 2, fib_i;
 
-	for (i = 2, n -= 1; i <= n; i++)
+	if (n >= 1)
+		printf("%llu", fib_x);
+
+	if (n >= 2)
+		printf(", %llu", fib_y);
+
+	for (int i = 3; i <= n; i++)
 	{
-		fib_i = fib_x + fib_y; /* get next sequence */
-		printf("%ld%s", fib_i, (i == n) ? "\n" : ", ");
-		/* swap values */
+		fib_i = fib_x + fib_y;
+		printf(", %llu", fib_i);
 		fib_x = fib_y;
 		fib_y = fib_i;
 	}
+
+	printf("\n");
 }
