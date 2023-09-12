5d5e3ea177d3bc3bffd2e2197323e67d61d7ff4a
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-12 19:00:45 +0000

    9X Tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,13 +6,19 @@
 */
 void times_table(void)
 {
-	int a, b, ans;
+	int a, b, ans, ext;
+
 	for (a = 48; a < 48 + 10; a++)
 	{
 		for (b = 48; b < 48 + 10; b++)
 		{
 			ans = (a - 48) * (b - 48);
 			ans += 48;
+			if (ans % 57 > 0)
+			{
+				ext = ans % 57;
+				_putchar('ext');
+			}
 			_putchar(ans);
 			if (b < 48 + 9)
 			{
