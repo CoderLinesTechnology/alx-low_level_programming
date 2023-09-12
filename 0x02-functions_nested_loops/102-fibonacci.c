0131b4df74583078f8f5d690786f91af68934bee
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-12 21:25:28 +0000

    Fab4

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,40 +1,28 @@
 #include <stdio.h>
-
-void print_fibonacci(size_t);
-
 /**
- * main - driver code
+ * main - main block
+ * Description: prints the first 50 Fibonacci numbers, starting with 1
+ * and 2, followed by a new line.
  * Return: 0
  */
-int main(void) {
-    printFibonacci(50);
-    return 0;
-}
-
-/**
- * print_fibonacci - print the n fibonacci number
- * @n: range
- */
-void printFibonacci(int n)
+int main(void)
 {
-	if (n <= 0)
- 		return;
+	int i = 0;
+	long int a = 0, b = 1, next;
 
-	unsigned long long fib_x = 1, fib_y = 2, fib_i;
-
-	if (n >= 1)
-		printf("%llu", fib_x);
-
-	if (n >= 2)
-		printf(", %llu", fib_y);
-
-	for (int i = 3; i <= n; i++)
+	while (i < 50)
 	{
-		fib_i = fib_x + fib_y;
-		printf(", %llu", fib_i);
-		fib_x = fib_y;
-		fib_y = fib_i;
-	}
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
 
-	printf("\n");
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
