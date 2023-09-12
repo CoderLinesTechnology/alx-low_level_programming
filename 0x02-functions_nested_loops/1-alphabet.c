1ecf9dd30d564ee1413b49ac6bc79d368d4ecd0f
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-12 11:55:41 +0000

    Task to print lower case alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*main - printing list of alphabet using _putchar
+*
+*Return: 0 (Success)
+*/
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,16 @@
+
+#include "main.h"
+/**
+*print_alphabet - printing list of alphabet using _putchar
+*
+*Return: 0 (Success)
+*/
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i++);
+	}
+}
