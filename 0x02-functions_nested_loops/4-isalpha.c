44a54b328dfcec79f88f1cd316c2cb1f07aba14b
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-12 14:05:15 +0000

    Check for letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*_isalpha - To check if character is alphabet or not
+*@c: Only parameter
+*Return: 1 if lower and 0 if not
+*/
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+
+}
