6986a01960429bdbddf0905a81096ae7f5d687ad
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-14 15:11:18 +0000

    Print from 0 -14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,19 +3,22 @@
 /**
 *more_numbers - print numbers
 *
-*
 */
 
 void more_numbers(void)
 {
-	int c = 0;
+	int x, c = 0;
 
-	while (c <= 14)
+	x = 0;
+	while (x < 10)
 	{
-		if ( c > 9)
-			_putchar('0' + c / 10);
-		_putchar('0' + c % 10);
-		c++;
+		while (c <= 14)
+		{
+			if ( c > 9)
+				_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+			c++;
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
