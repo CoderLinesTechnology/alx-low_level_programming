b8f9a28ab73a15a1866624f960c760ceee921112
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-14 21:39:34 +0000

    Print numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,14 +12,17 @@ void print_number(int n)
 	unsigned int m = n;
 
 	if (n < 0)
+	{
+		_putchar('-');
 		m = -n;
-	if (m == 0)
+	}
+	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-
-	print_number(m / 10);
+	if (m / 10)
+		print_number(m / 10);
 	_putchar(m % 10 + '0');
 }
 
