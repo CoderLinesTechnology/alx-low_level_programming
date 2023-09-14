765c184153f838dd6139a92a1ab421c959f4c4f8
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-14 14:36:08 +0000

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+*print_numbers - print numbers
+*
+*Return: the result
+*/
+
+void print_numbers(void)
+{
+	int c = 48;
+
+	while (c <= 57)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
