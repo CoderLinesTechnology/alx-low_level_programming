4588bc420e55904b84924287927b982919b3740a
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-21 23:48:03 +0000

    STring concat with length

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-*_strcat - String concatenation
+*_strncat - String concatenation
 *@dest: first param
 *@src: source param
 *@n: number of bytes
 *Return: string
 */
 
-char *_strcat(char *dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int size = 0;
 
