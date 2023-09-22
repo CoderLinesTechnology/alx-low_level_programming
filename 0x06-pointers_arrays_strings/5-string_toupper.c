12fb43ba18d09807819b38c0b8b5f52432b9638d
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-22 00:54:55 +0000

    Convert to upper case task

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*string_toupper - convert to upper case
+*@string: param
+*Return: uppercas
+*/
+
+char *string_toupper(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		if (string[i] > 'a' && string[i] < 'z')
+			string[i] = string[i] - 32;
+	return (string);
+}
