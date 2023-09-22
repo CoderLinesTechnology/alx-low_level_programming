30a8af57e03b39cb2f986927aa33acc424853e8d
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-22 20:17:04 +0000

    Encode

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*leet - Decode specific characters
+*@s: first param
+*
+*Return: encoded
+*/
+
+char *leet(char *s)
+{
+	char c[5][3] =  {{'a', 'A', '4'}, {'e', 'E', '3'},
+	{'o', 'O', '0'}, {'t', 'T', '7'}, {'l', 'L', '1'}};
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++}
+	{
+		if (s[i] == c[i][0] || s[i] == c[i][1])
+		{
+			s[i] = c[i][2];
+		}
+	}
+	return (s);
+}
