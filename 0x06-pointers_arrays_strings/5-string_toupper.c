14c639f1bfbadb642fd9626ed36cf7141d30da04
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-22 00:56:59 +0000

    Convert to upper case task

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,7 +11,7 @@ char *string_toupper(char *string)
 	int i;
 
 	for (i = 0; string[i] != '\0'; i++)
-		if (string[i] > 'a' && string[i] < 'z')
+		if (string[i] >= 'a' && string[i] <= 'z')
 			string[i] = string[i] - 32;
 	return (string);
 }
