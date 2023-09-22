509c86f12d59466f35d659ec5eec8fb29c00a5d3
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-22 00:31:06 +0000

    Reverse Array Task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.j"
+
+/**
+*reverse_array - Function to reverse the element of an array
+*@a: array
+*@n: array size
+*
+*/
+
+void reverse_array(int *a, int n)
+{
+	int temp, i = 0;
+
+	while (i < n)
+	{
+		temp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = temp;
+
+		i++;
+		n--;
+	}
+	return (a);
+}
