2a3d668bfc2a42be7b3258c99690dbb17d76ef25
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-22 19:29:58 +0000

    Sentece case task

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,23 +1,31 @@
 #include "main.h"
 
 /**
-*cap_string - convert to upper case
-*@s: param
-*Return: uppercas
-*/
-
+ * cap_string - Convert the first letter of each word to uppercase.
+ * @s: The input string.
+ * Return: Pointer to the modified string.
+ */
 char *cap_string(char *s)
 {
-	int i;
+    int i;
 
-	if (s[0] >= 'a' && s[0] <= 'z')
-	s[0] = s[0] - 32;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+        {
+            s[i] -= ('a' - 'A');
+        }
+        else if (s[i] == ' ' || s[i] == '\n' || s[i] == ',' || s[i] == ';' || s[i] == '.' ||
+                 s[i] == '!' || s[i] == '?' || s[i] == '(' || s[i] == ')' || s[i] == '{' ||
+                 s[i] == '}' || s[i] == '\t')
+        {
+            if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+            {
+                s[i + 1] -= ('a' - 'A');
+            }
+        }
+    }
 
-	for (i = 1; s[i] != '\0'; i++)
-		if (s[i] == '\n' || s[i] == ' ' || s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!'
-		|| s[i] == '?' || s[i] == '(' || s[i] == ')' || s[i] == '{'
-		|| s[i] == '\t' || s[i] == '}')
-			if (s[++i] >= 'a' && s[i] <= 'z')
-				s[i] = s[i] - 32;
-	return (s);
+    return s;
 }
+
