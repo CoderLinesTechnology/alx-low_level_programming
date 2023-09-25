1d33a98cf511ef30974a1569b0bc1c262beb6512
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-25 23:39:53 +0000

    Add up task

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*print_diagsums - Print diaginal sums
+*@a: first param
+*@size: second param
+*/
+
+void print_diagsums(int *a, int size)
+{
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		sum1 += a[i * size + i];
+		sum2 += a[i * size + (size - 1 - i)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
