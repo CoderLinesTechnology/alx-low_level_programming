1d269841a237f43c84a3137de89365b656c9fa92
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-25 22:26:52 +0000

    String Span Task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,7 +15,7 @@ unsigned int _strspn(char *s, char *accept)
 	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] != 32)
-			return (x)
+			return (x);
 		for (j = 0; accept[j] >= '\0'; j++)
 		{
 			if (s[i] == accept[j])
