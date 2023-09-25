23b046131472769dc4023880250cc1d516b267f3
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-25 22:50:57 +0000

    String Break Task

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *_strpbrk - Substring break
+ *@s: string parameter
+ *@accept: character search parameter
+ *
+ *Return: number of bytes
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	int x = 0;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] > '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				return (s[i]);
+		}
+	}
+	return (0);
+}
