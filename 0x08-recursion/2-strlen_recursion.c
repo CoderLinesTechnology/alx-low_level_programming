dd8ff407d39150b65df59d279f3ca37009925598
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-27 18:53:23 +0000

    STring length task

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - Check string length
+ *@s: String param
+ *Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	int i =0;
+
+	if (*s == '\0')
+	{
+		return (i);
+	}
+	_strlen_recursion(i++);
+}
