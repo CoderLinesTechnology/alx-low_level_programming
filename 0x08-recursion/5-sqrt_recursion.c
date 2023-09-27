853a09139f3228e3c41d3e77bda33a38fdd3c0a5
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-27 19:54:49 +0000

    Square root task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * find_sqrt - Helper function to find the
+ * natural square root of a number using binary search
+ * @n: The number for which to find the square root
+ * @low: The lower bound of the search range
+ * @high: The upper bound of the search range
+ *
+ * Return: The natural square root of the number,
+ * or -1 if no natural square root exists
+ */
+int find_sqrt(int n, int low, int high)
+{
+	if (low > high)
+		return (-1);
+
+	int mid = (low + high) / 2;
+	int square = mid * mid;
+
+	if (square == n)
+		return (mid);
+	else if (square < n)
+		return (find_sqrt(n, mid + 1, high));
+	else
+		return (find_sqrt(n, low, mid - 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: The number for which to find the square root
+ *
+ * Return: The natural square root of the number,
+ * or -1 if no natural square root exists
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1); /* Negative numbers don't have natural square roots */
+
+	/* Call the helper function with appropriate initial values */
+	return (find_sqrt(n, 0, n));
+}
