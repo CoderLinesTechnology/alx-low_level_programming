bf8f6bd5840fd911c001cee6552c5378bf0e94d1
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-27 20:14:50 +0000

    Square root task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,41 +1,24 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
- * _sqrt - Helper function to find the
- * natural square root of a number using binary search
+ * _sqrt_recursion - Returns the natural square root of a number using Newton-Raphson method
  * @n: The number for which to find the square root
  *
- * Return: The natural square root of the number,
- * or -1 if no natural square root exists
+ * Return: The natural square root of the number, or -1 if no natural square root exists
  */
-int _sqrt(int n)
-{
-	int low, high, mid;
-	long square;
-
-	if (n < 0)
-	return -1; /* Negative numbers don't have natural square roots */
-
-	low = 0;
-	high = n;
-
-	while (low <= high)
-	{
-	mid = low + (high - low) / 2;
-	square = (long)mid * mid;
-
-		if (square == n)
-			return mid; /* Found the exact square root */
-		else if (square < n)
-			low = mid + 1; /* Continue searching in the upper half */
-		else
-			high = mid - 1; /* Continue searching in the lower half */
-	}
-
-	return -1; /* No natural square root exists */
-}
 
 int _sqrt_recursion(int n)
 {
-	return _sqrt(n);
+    if (n < 0)
+        return -1; /* Negative numbers don't have natural square roots */
+
+    if (n == 0 || n == 1)
+        return n; /* Square root of 0 or 1 is the number itself */
+
+    double x0 = n; // Initial guess
+    double x1 = 0.5 * (x0 + n / x0); // Newton-Raphson method formula
+
+    if (x0 == x1) // Check for convergence
+        return (int)x0;
+
+    return _sqrt_recursion((int)x1); // Recursively call with the new guess
 }
