948e7551c01d268a93c65181648baccf38bd4fdb
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-29 20:15:01 +0000

    Print number of arguments task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - Print the number of arguments passed
+*@argc: argument count
+*@argv: argument variable
+*
+*Return: Success
+*/
+
+int main(int argc, char* argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
