3b1a657ee4e6d32734713003da109cbfa8f6be08
CoderLinesTechnology <stephenbedz@gmail.com>
2023-09-29 21:09:30 +0000

    Multiply taks

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,9 +13,9 @@ int main(int argc, char *argv[])
 {
 	int i = 0;
 
-	if ( argc == 3)
+	if (argc == 3)
 	{
-		i = atoi(argv[1]) * atoi(argv [2]);
+		i = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", i);
 	}
 	else
