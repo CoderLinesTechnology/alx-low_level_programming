5526ecd802ff8850920166b12a7887124b7f5186
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-03 23:49:51 +0000

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -9,52 +9,41 @@
  */
 char **strtow(char *str)
 {
-    char **words;
-    int i, j, k, len, word_count;
+	char **words;
+	int i, j, k, len, word_count;
 
-    if (str == NULL || *str == '\0')
-        return (NULL);
+	if (str == NULL || *str == '\0')
+		return (NULL);
+	word_count = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
+			word_count++;
+	if (word_count == 0)
+		return (NULL);
+	words = (char **)malloc((word_count + 1) * sizeof(char *));
+	if (words == NULL)
+		return (NULL);
+	for (i = 0, k = 0; i < word_count; i++)
+	{
+		while (str[k] == ' ')
+			k++;
+		len = 0;
+		while (str[k + len] != ' ' && str[k + len] != '\0')
+			len++;
+		words[i] = (char *)malloc((len + 1) * sizeof(char));
+		if (words[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(words[j]);
+			free(words);
+			return (NULL);
+		}
+		for (j = 0; j < len; j++)
+			words[i][j] = str[k + j];
+		words[i][j] = '\0';
 
-    word_count = 0;
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] != ' ' && (i == 0 || str[i - 1] == ' '))
-            word_count++;
-    }
-
-    if (word_count == 0)
-        return (NULL);
-
-    words = (char **)malloc((word_count + 1) * sizeof(char *));
-    if (words == NULL)
-        return (NULL);
-
-    for (i = 0, k = 0; i < word_count; i++)
-    {
-        while (str[k] == ' ')
-            k++;
-
-        len = 0;
-        while (str[k + len] != ' ' && str[k + len] != '\0')
-            len++;
-
-        words[i] = (char *)malloc((len + 1) * sizeof(char));
-        if (words[i] == NULL)
-        {
-            for (j = 0; j < i; j++)
-                free(words[j]);
-            free(words);
-            return (NULL);
-        }
-
-        for (j = 0; j < len; j++)
-            words[i][j] = str[k + j];
-        words[i][j] = '\0';
-
-        k += len;
-    }
-
-    words[word_count] = NULL;
-    return (words);
+		k += len;
+	}
+	words[word_count] = NULL;
+	return (words);
 }
-
