dd80529489a38c7e27b453c93ccaaa63ce34c170
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-03 23:09:08 +0000

    Allocation Task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,35 +1,43 @@
+#include "main.h"
+#include <string.h>
 #include <stdlib.h>
 
 /**
- * alloc_grid - Allocates a 2D grid of integers.
- * @width: Width of the grid.
- * @height: Height of the grid.
+ * alloc_grid - Returns a pointer to a 2D grid of integers
+ * initialized with zero
+ * @width: Width of grid
+ * @height: Height of grid
  *
- * Return: Pointer to the allocated 2D grid of integers,
+ * Return: A pointer to the 2D grid of integers, NULL if it fails
  */
 int **alloc_grid(int width, int height)
 {
 	int **grid;
-	int i, j;
+	int row, col;
 
-	if (width <= 0 || height <= 0)
+	if (height <= 0 || width <= 0)
 		return (NULL);
 
-	grid = malloc(height * sizeof(int *));
+	grid = (int **)malloc(height * sizeof(int *));
 	if (grid == NULL)
 		return (NULL);
 
-	for (i = 0; i < height; i++)
+	for (row = 0; row < height; row++)
 	{
-		grid[i] = malloc(width * sizeof(int));
-		if (grid[i] == NULL)
-			for (j = 0; j < i; j++)
-				free(grid[j]);
-		free(grid);
-		return (NULL);
+		grid[row] = (int *)malloc(width * sizeof(int));
+		if (grid[row] == NULL)
+		{
+			for (int i = 0; i < row; i++)
+			{
+				free(grid[i]);
+			}
+			free(grid);
+			return (NULL);
+		}
+
+		memset(grid[row], 0, width * sizeof(int));
 	}
-	for (j = 0; j < width; j++)
-		grid[i][j] = 0;
 
 	return (grid);
 }
+
