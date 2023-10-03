f9ecc3e4975fd6f4261d828e4cd8860eef48cab3
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-03 23:26:20 +0000

    I will show you how great I am

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,43 +1,36 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * argstostr - Concatenates all the arguments of the program.
- * @ac: Number of arguments
- * @av: Array of arguments
- *
- * Return: A pointer to a new string containing concatenated arguments
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, total_length = 0;
-	int newline_count = ac - 1;
-	char *concatenated_args, *ptr;
+	int i, n, r = 0, l = 0;
+	char *str;
 
-	if (ac <= 0 || av == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
-		for (j = 0; av[i][j] != '\0'; j++)
-			total_length++;
+		for (n = 0; av[i][n]; n++)
+			l++;
+	l += ac;
 
-	concatenated_args = (char *)malloc(total_length + newline_count + 1);
-
-	if (concatenated_args == NULL)
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
 		return (NULL);
-
-	ptr = concatenated_args;
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			*ptr++ = av[i][j];
-
-		if (i < ac - 1)
-			*ptr++ = '\n';
+		for (n = 0; av[i][n]; n++)
+		{
+			str[r] = av[i][n];
+			r++;
+		}
+		if (str[r] == '\0')
+			str[r++] = '\n';
 	}
-
-	*ptr = '\0';
-
-	return (concatenated_args);
+	return (str);
 }
-
