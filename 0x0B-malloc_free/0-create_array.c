846bdc8d26d2c3a2eef5bfd703c56549188c7bda
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-03 20:03:10 +0000

    Create a char

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+/**
+*create_array - Create an array with size specified
+*@size: Size of array
+*@c: char
+*
+*Return: Pointer
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *i;
+	int x = 0;
+
+	i = malloc(size);
+	if(size == 0)
+		return (NULL);
+	for (;x < size;)
+	{
+		i[x] = c;
+		x++;
+	}
+	return (i);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Read me for Malloc and Free
