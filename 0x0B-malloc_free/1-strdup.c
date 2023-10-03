ece28063f389a04797b976e9db4cebd623766f17
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-03 22:32:47 +0000

    Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+*_strdup - Copy a string given in the parameter
+*@str: First string
+*
+*Return: a pointer to a newly allocated space in memory
+*/
+
+char *_strdup(char *str)
+{
+	char *c;
+
+	if (str == NULL)
+		return (NULL);
+
+	c = malloc((strlen(str) + 1) * sizeof(c));
+	if (c == NULL)
+		return (NULL);
+
+	strcpy(c, str);
+
+	return (c);
+}
