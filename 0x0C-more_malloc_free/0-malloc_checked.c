cfead8f710c822839743fef4f9a6b89f17e5309d
Stephen Selorm Bedzrah <stephenbedz@gmail.com>
2023-10-09 10:14:31 -0700

    Check malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - check if malloc propoerly works
+ * @b: first param
+ *
+ * Return: a ponter to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *prt;
+
+	prt = malloc(sizeof(b));
+	if (prt == NULL)
+		exit(98);
+
+	*prt = b;
+}
