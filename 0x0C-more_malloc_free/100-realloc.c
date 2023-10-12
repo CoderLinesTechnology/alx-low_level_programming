8fbc42a9a9e7285a7bdaadda655f7446caedf70c
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-12 08:26:11 +0000

    Task for Reallocate Memory

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,7 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr != NULL)
 	{
 		min_size = (old_size < new_size) ? old_size : new_size;
-		for (x = 0; x < min_size; ++x)
+		for (x = 0; x < min_size; x++)
 			((char *)new_ptr)[x] = ((char *)ptr)[x];
 		free(ptr);
 	}
