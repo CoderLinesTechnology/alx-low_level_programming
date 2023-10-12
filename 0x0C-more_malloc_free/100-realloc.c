8c1e41f12dd1a975b5bc8f12067aa809bafbd77e
CoderLinesTechnology <49565920+CoderLinesTechnology@users.noreply.github.com>
2023-10-12 12:20:20 +0000

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	new_ptr = malloc(new_size);
 
-	if (new_ptr != NULL)
+	if (new_ptr == NULL)
+		return (NULL);
+	if (ptr != NULL)
 	{
 		min_size = (old_size < new_size) ? old_size : new_size;
 		for (x = 0; x < min_size; ++x)
