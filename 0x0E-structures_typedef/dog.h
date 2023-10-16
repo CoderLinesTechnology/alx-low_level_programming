55f6c8b7b0a61039787131383ff9802f934c9223
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-16 11:47:36 +0000

    Poppy Task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,26 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
-
+/**
+ * struct dog - blueprint for dog entity
+ * @name: name of dog
+ * @owner: name of dog's owner
+ * @age: dog's age
+ */
 struct dog
 {
 	char *name;
-	float age;
 	char *owner;
-}
-
-typedef struct dog dog_t;
+	float age;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/* set new type definition */
+typedef struct dog dog_t;
+
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
 
 #endif
