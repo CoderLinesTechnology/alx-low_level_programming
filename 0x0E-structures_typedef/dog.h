8b0f988bee71f82cec75c7f33680b8ec908418c3
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-16 12:09:54 +0000

    Init dog task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,10 +17,11 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-/* set new type definition */
 typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 #endif
