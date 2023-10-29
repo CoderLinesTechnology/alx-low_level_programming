a1ca9fc2443b1475002fbd1ddff64226c4e07def
CoderLinesTechnology <stephenbedz@gmail.com>
2023-10-29 17:04:24 +0000

    Set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
-		*n |= (1ul << index); /* Set the bit at the specified index to 1 */
+	*n |= (1ul << index); /* Set the bit at the specified index to 1 */
 
 	return (1);
 }
